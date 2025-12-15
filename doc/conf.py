08ecd7e4c4d2f4211edbc728ee04e2ffd7a15e2d
bilhalva01 <bilhalva01@gmail.com>
2025-12-15 16:01:08 -0300

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,7 @@ extensions = ['myst_parser']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+language = 'pt'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
